ca016b803b8805506cee19622008e105fca9e07b
javaIsGf=== <85638363+surajxx159357@users.noreply.github.com>
2023-10-26 07:29:10 +0530

    Update Lms.java

--- model/Lms.java
+++ model/Lms.java
@@ -1,12 +1,1 @@
-package edu.qsp.lms.model;
 
-public class Lms {
-	
-	public static String lmsName="qsplms";
-	public static String lmspwd="qsp@123";
-	public static void main(String[] args)
-	{
-//		Student s1=new Student();
-
-	}
-}
