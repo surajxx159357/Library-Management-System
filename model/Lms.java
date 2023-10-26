4349548ad87de5a6ec85b7ce968234e71b90714e
javaIsGf=== <85638363+surajxx159357@users.noreply.github.com>
2023-10-26 07:26:04 +0530

    Add files via upload

---
+++ model/Lms.java
@@ -0,0 +1,12 @@
+package edu.qsp.lms.model;
+
+public class Lms {
+	
+	public static String lmsName="qsplms";
+	public static String lmspwd="qsp@123";
+	public static void main(String[] args)
+	{
+//		Student s1=new Student();
+
+	}
+}
